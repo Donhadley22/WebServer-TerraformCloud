c01c56b3c08f5db301859689d2e824039d2437fe
Donhadley <donhadleygirlandchika@gmail.com>
2025-05-29 22:22:54 +0100

    modifiet outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -5,4 +5,10 @@ output "ec2_instance_id" {
 output "ec2_public_ip" {
   value = module.ec2.public_ip
 }
+output "vpc_id" {
+  value = module.vpc.vpc_id
+}
+output "ec2_public_dns" {
+  value = module.ec2.public_dns
+}
 
