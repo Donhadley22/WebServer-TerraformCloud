55a934e2d5fa0a77919560b5077550caed89bc2e
Donhadley <donhadleygirlandchika@gmail.com>
2025-05-29 22:28:56 +0100

    modified outputs.tf and terraform.yml

--- outputs.tf
+++ outputs.tf
@@ -5,10 +5,16 @@ output "ec2_instance_id" {
 output "ec2_public_ip" {
   value = module.ec2.public_ip
 }
+
 output "vpc_id" {
   value = module.vpc.vpc_id
 }
+
 output "ec2_public_dns" {
   value = module.ec2.public_dns
+}
+
+output "ec2_instance_state" {
+  value = module.ec2.instance_state
 }
 
